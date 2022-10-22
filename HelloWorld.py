b60299a0b5044b3b03374bb164495bae5ed4e4f1
Xaeon1 <alex.pironkova@gmail.com>
2022-10-22 14:31:58 +0300

    Variables + input + if-else learned.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,26 @@
+"""
 print("Hello World!")
-
 print("o----")
 print(" |||| ")
 print("*" * 10)
+"""
+
+"""
+price = 10  #int
+rating = 4.9    #float
+name = "Alex"   #String
+is_published = True #Boolean
+print(price)
+"""
+
+# Ex.1 We check in a patient named John Smith.
+#      He's 20 years old and is a new patient.
+
+input_full_name = input("Input patient's name: ")
+input_age = int(input("Input patient's age: "))
+is_existing_patient = False
+
+if input_full_name == "John Smith" and is_existing_patient == False:
+    print("Check in successful...")
+else:
+    print("This patient is already checked in...")
