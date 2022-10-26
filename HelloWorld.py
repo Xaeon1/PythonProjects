4393472c743aba8cf4570653f8520334138c9245
Xaeon1 <alex.pironkova@gmail.com>
2022-10-26 09:38:48 +0300

    Uni lab exercise

--- HelloWorld.py
+++ HelloWorld.py
@@ -20,7 +20,9 @@ input_full_name = input("Input patient's name: ")
 input_age = int(input("Input patient's age: "))
 is_existing_patient = False
 
-if input_full_name == "John Smith" and is_existing_patient == False:
+if input_full_name == "John Smith" and is_existing_patient is False:
     print("Check in successful...")
 else:
     print("This patient is already checked in...")
+
+#
---
+++ Lab_ex_1.py
@@ -0,0 +1,40 @@
+# EX.1 Count syllables from user input
+
+"""
+user_input = input("Input text \n")
+
+frequency = {}
+
+for el in user_input:
+    frequency[el] = user_input.count(el)
+
+print(frequency)
+"""
+
+# Second solution
+"""
+def check_freq(x):
+    return {c: x.count(c) for c in set(x)}
+"""
+
+# EX.2
+
+user_input = int(input("Enter length of array: "))
+
+array = []
+dictionary = {}
+
+for i in range(1, user_input+1):
+    array.append(i)
+
+array.sort(reverse=True)
+
+for i in range(1,user_input+1):
+    dictionary[i] = array[i-1]
+
+print(dictionary)
+"""
+for el in array:
+    dictionary.key = el
+"""
+
